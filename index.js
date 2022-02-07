7a4dfd21b390572eb8191e2bb2a5632cac7ac60b
vandu <hoang.duong@iem.thm.de>
2022-02-07 10:57:28 +0100

    dropdown menu

--- index.js
+++ index.js
@@ -37,7 +37,20 @@ if (window.innerWidth > 500) {
 
 }
 
+document.addEventListener("click", e=>{
+    const isDropdownButton = e.target.matches("[data-dropdown-button]")
+    if(!isDropdownButton && e.target.closest("[data-dropdown]") != null) return
+    let currentDropdown
+    if(isDropdownButton){
+    currentDropdown = e.target.closest('[data-dropdown]')
+        currentDropdown.classList.toggle('active')
+    }
+    document.querySelectorAll("[data-dropdown].active").forEach(dropdown =>{
+        if (dropdown=== currentDropdown) return
+        dropdown.classList("active")
+    })
 
+})
 
 
 
